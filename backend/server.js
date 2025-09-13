45f4dc601bd8341daae7429955e90dc02a82403c
Mohammad Fayaz <mohammadfayaz@Mohammads-MacBook-Air.local>
2025-09-13 16:53:42 +0530

    Serve React build from backend

--- backend/server.js
+++ backend/server.js
@@ -72,7 +72,7 @@ if (typeof providerRoutes === 'function') {
 //my code
 const __dirname = path.resolve();
 app.use(express.static(path.join(__dirname, "frontend/build")));
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
 res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
 });
 
